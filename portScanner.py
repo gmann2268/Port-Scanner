5698028ddf581aab4d16669f971ffd558125869c
gmann2268 <gireshjeet22@gmail.com>
2024-01-22 19:14:26 +0800

    Created allPortScan function
    Scans ports 1 to 1024

--- portScanner.py
+++ portScanner.py
@@ -9,6 +9,22 @@ def connectionScan(targetHost, targetPort):
     except:
         print("[-]%d/tcp closed"% targetPort)
 
+def allPortScan(targetHost):
+    try:
+        targetIP = gethostbyname(targetHost)
+    except:
+        print("[-] Cannot resolve %s"% targetHost)
+        return
+    try:
+        targetName = gethostbyaddr(targetIP)
+        print("\n[+] Scan result of: %s"% targetName[0])
+    except:
+        print("\n[+] Scan result of: %s"% targetIP)
+    setdefaulttimeout(1)
+    for targetPort in range(1, 1025):
+        print("Scanning Port: %d"% targetPort)
+        connectionScan(targetHost, targetPort)
+
 def portScan(targetHost, targetPorts):
     try:
         targetIP = gethostbyname(targetHost)
@@ -24,6 +40,7 @@ def portScan(targetHost, targetPorts):
     for targetPort in targetPorts:
         print("Scanning Port: %d"% targetPort)
         connectionScan(targetHost, int(targetPort))
+
 
 if __name__ == '__main__':
     portScan("google.com", [80, 22])
